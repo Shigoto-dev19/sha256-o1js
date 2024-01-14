a5b36e1e2c004cef62b478d24387b41f49de817a
Shigoto-dev19 <shigoto-dev19@protonmail.com>
2024-01-14 15:57:25 +0100

    Add o1js SHA3_256 as a comparator in the benchmarks

--- src/benchmarks/benchmark.ts
+++ src/benchmarks/benchmark.ts
@@ -6,6 +6,15 @@ import {
   nobleHash,
   generateRandomInput,
 } from '../test-utils.js';
+import { Hash, Bytes } from 'o1js';
+
+class Bytes32 extends Bytes(32) {}
+function o1jsSha3_256(input: string) {
+  const parsedInput = Bytes32.fromString(input);
+  const sha3Digest = Hash.SHA3_256.hash(parsedInput);
+  
+  return sha3Digest
+}
 
 const { mark, compare, run } = bench;
 
@@ -20,6 +29,7 @@ run(async () => {
   );
 
   await compare(`\nSHA256 Benchmarks => ${iterations} iterations`, iterations, {
+    o1jsSha3_256: () => o1jsSha3_256(generateRandomInput(33)),
     o1jsSha256Circom: () => o1jsHashCircom(randomInputGenerator()),
     o1jsSha256Gadgets: () => o1jsHash(randomInputGenerator()),
     nodeSha256: () => nodeHash(randomInputGenerator()),
